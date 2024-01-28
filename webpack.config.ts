17cf2f16f29fed31c6e05bf2323d8da6446c1893
freecree <bohdansidorov@gmail.com>
2024-01-28 10:28:35 +0200

    refactor: remove unused sass packages

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,56 +1,52 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
-import 'webpack-dev-server';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import "webpack-dev-server";
 
 interface EnvVariables {
-  mode: 'production' | 'development',
-  port: number 
-
+  mode: "production" | "development";
+  port: number;
 }
 
 export default (env: EnvVariables) => {
   const config = {
-    mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js',
-      clean: true
-
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+      clean: true,
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          loader: 'ts-loader'
+          loader: "ts-loader",
         },
         {
-          test: /\.s[ac]ss$/i,
+          test: /\.css$/i,
           use: [
             // Creates `style` nodes from JS strings
             "style-loader",
             // Translates CSS into CommonJS
             "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
           ],
         },
-      ]
+      ],
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js']
+      extensions: [".tsx", ".ts", ".js"],
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src', 'index.html')
-      })
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
     ],
-    devtool: 'inline-source-map',
+    devtool: "inline-source-map",
     devServer: {
-      static: path.resolve(__dirname, 'dist'),
-      port: env.port ?? 3000
-    }
-  }
+      static: path.resolve(__dirname, "dist"),
+      port: env.port ?? 3000,
+    },
+  };
   return config;
 };
