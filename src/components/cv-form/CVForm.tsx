268728423b199e396a9db34d9079cbde53fb161e
freecree <bohdansidorov@gmail.com>
2024-01-30 19:04:25 +0200

    feat: add education block layout

--- src/components/cv-form/CVForm.tsx
+++ src/components/cv-form/CVForm.tsx
@@ -1,16 +1,18 @@
 import { Form } from 'antd';
 import PersonalInfo from '../personal-info/PersonalInfo';
+import Education from '../education/Education';
 
 function CVForm() {
   return (
     <Form
       name='cv-form'
-      style={{ marginTop: '20px' }}
+      style={{ marginTop: '20px', paddingBottom: '50px' }}
       initialValues={{ remember: true }}
       autoComplete='off'
       layout='vertical'
     >
       <PersonalInfo />
+      <Education />
     </Form>
   );
 }
